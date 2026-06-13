9656f48b8e514e7e6c239593f8f980d499f128cd
sebastian <117323429+sebastianGallardoJ@users.noreply.github.com>
2026-06-12 21:58:23 -0300

    se suben cambios y complementos

--- OcurrenceCounter.cpp
+++ OcurrenceCounter.cpp
@@ -38,13 +38,7 @@ namespace UndavOcurrenceCounter {
     }
     void DestroyOcurrenceCounter(OcurrenceCounter* counter) {
         delete[] counter->ocurrencias;
-<<<<<<< HEAD
-        delete counter;
-    }
-}
-=======
         UndavAlphabet::DestroyAlphabet(counter->alphabet);
         delete counter;
     }
 }
->>>>>>> de8404f18877e2ce58efe18d148bbeab679c643e
